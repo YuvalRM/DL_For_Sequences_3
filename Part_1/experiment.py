078a1aa3e1aa7cb046cc3c5def99446925b9b510
I583925 <guy.ben.razon@sap.com>
2024-07-24 10:26:44 +0300

    add challenge.pdf

--- Part_1/experiment.py
+++ Part_1/experiment.py
@@ -3,6 +3,11 @@ from torch.utils.data import DataLoader, TensorDataset
 
 PADDING = 0
 batch_size = 32
+lr = 0.001
+epochs = 100
+char_embed_size = 10
+hidden_size1 = 100
+hidden_size2 = 50
 
 
 def get_char_to_idx():
@@ -11,7 +16,7 @@ def get_char_to_idx():
     return char_to_idx
 
 
-def get_X_y():
+def get_dataset():
     char_to_idx = get_char_to_idx()
     with open('pos_examples', 'r') as pos_file:
         pos_examples = pos_file.readlines()
@@ -40,11 +45,9 @@ def split_dataset(X, y, train_ratio, test_ratio):
 
 
 class LSTM(torch.nn.Module):
-    def __init__(self, char_embed_size, num_char_embedding=14, hidden_size1=100, hidden_size2=50, num_layers=1,
-                 batch_size=32):
+    def __init__(self, char_embed_size, num_char_embedding=14, hidden_size1=100, hidden_size2=50, batch_size=32, num_layers=1):
         super(LSTM, self).__init__()
         self.hidden_size = hidden_size1
-        self.num_layers = num_layers
         self.char_embedding = torch.nn.Embedding(num_char_embedding, char_embed_size)
         self.lstm = torch.nn.LSTM(char_embed_size, hidden_size1, num_layers,
                                   batch_first=True)
@@ -66,19 +69,33 @@ class LSTM(torch.nn.Module):
 
 class LstmTrainer:
 
-    def __init__(self, epochs=100):
+    def __init__(self, epochs=100, char_embed_size=10, hidden_size1=100, hidden_size2=50, num_layers=1, batch_size=32,
+                 lr=0.001):
         self.epochs = epochs
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = None
+        self.criterion = None
+        self.char_embed_size = char_embed_size
+        self.hidden_size1 = hidden_size1
+        self.hidden_size2 = hidden_size2
+        self.num_layers = num_layers
+        self.batch_size = batch_size
+        self.lr = lr
 
     def train(
             self, X: torch.Tensor, y: torch.Tensor
     ) -> LSTM:
 
         train_loader, val_loader = self.get_train_val_loaders(X, y)
-        self.model = LSTM(10)
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+
+        self.model = LSTM(char_embed_size=self.char_embed_size,
+                          hidden_size1=self.hidden_size1,
+                          hidden_size2=self.hidden_size2,
+                          batch_size=self.batch_size).to(self.device)
+
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         self.criterion = torch.nn.CrossEntropyLoss()
+        best_val_loss = torch.inf
 
         for epoch in range(self.epochs):
             self.model.train()
@@ -93,12 +110,16 @@ class LstmTrainer:
                 optimizer.step()
 
             val_loss, val_accuracy = self.validate(val_loader)
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                torch.save(self.model.state_dict(), 'best_model.pth')
+
             train_loss /= len(train_loader)
 
             print(
                 'Epoch: {}, Train loss: {:5f}, Val loss: {:5f}, Val accuracy: {:5f}'.format(epoch, train_loss, val_loss,
                                                                                             val_accuracy))
-
+        self.model.load_state_dict(torch.load('best_model.pth'))
         return self.model
 
     def validate(self, data_loader):
@@ -138,7 +159,7 @@ class LstmTrainer:
 
 
 if __name__ == "__main__":
-    X, y = get_X_y()
+    X, y = get_dataset()
     X_train, X_test, y_train, y_test = split_dataset(X, y, 0.8, 0.2)
 
     trainer = LstmTrainer()
